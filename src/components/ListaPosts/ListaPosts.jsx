c076805249da9c4fae4b7f05e3c474fd8c414066
Vinils76 <antoniovinicius7670@gmail.com>
2022-10-21 11:51:50 -0300

    conteudo

--- src/components/ListaPosts/ListaPosts.jsx
+++ src/components/ListaPosts/ListaPosts.jsx
@@ -29,34 +29,14 @@ const ListaPosts = () => {
 
   return (
     <div className={estilos.lista_posts}>
-      <article className={estilos.post}>
-        <h3>Título do post...</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-          dolorem facere quam mollitia eligendi dolor?
-        </p>
-      </article>
-      <article className={estilos.post}>
-        <h3>Título do post...</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-          dolorem facere quam mollitia eligendi dolor?
-        </p>
-      </article>
-      <article className={estilos.post}>
-        <h3>Título do post...</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-          dolorem facere quam mollitia eligendi dolor?
-        </p>
-      </article>
-      <article className={estilos.post}>
-        <h3>Título do post...</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-          dolorem facere quam mollitia eligendi dolor?
-        </p>
-      </article>
+      {posts.map((post) => {
+        return (
+          <article className={estilos.post}>
+            <h3>{post.titulo}</h3>
+            <p>{post.subtitulo}</p>
+          </article>
+        );
+      })}
     </div>
   );
 };
