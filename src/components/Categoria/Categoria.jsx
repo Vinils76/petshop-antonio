61a2ba6193198c6f600ad5c7f5131c414bb8df5c
Vinils76 <antoniovinicius7670@gmail.com>
2022-11-01 08:55:49 -0300

    updates

--- src/components/Categoria/Categoria.jsx
+++ src/components/Categoria/Categoria.jsx
@@ -1,9 +1,16 @@
+import { useParams } from "react-router-dom";
+import ListaCategorias from "../ListaCategorias/ListaCategorias";
+import ListaPosts from "../ListaPosts/ListaPosts";
 import estilos from "./Categoria.module.css";
 
 const Categoria = () => {
+  const { nome } = useParams();
   return (
     <section>
       <h2 className={estilos.titulo_secao}>Categorias</h2>
+
+      <ListaCategorias />
+      <ListaPosts url={`posts?categoria=${nome}`} />
     </section>
   );
 };
--- src/components/ListaPosts/ListaPosts.jsx
+++ src/components/ListaPosts/ListaPosts.jsx
@@ -4,7 +4,7 @@ import serverApi from "../../api/servidor-api";
 import ArtigoPost from "../ArtigoPost/ArtigoPost";
 import LoadingDesenho from "../LoadingDesenho/LoadingDesenho";
 import estilos from "./ListaPosts.module.css";
-const ListaPosts = () => {
+const ListaPosts = (props) => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -15,7 +15,7 @@ const ListaPosts = () => {
   useEffect(() => {
     async function getPosts() {
       try {
-        const resposta = await fetch(`${serverApi}/posts`);
+        const resposta = await fetch(`${serverApi}/${props.url}`);
         const dados = await resposta.json();
         setPosts(dados);
         setLoading(false);
@@ -24,7 +24,7 @@ const ListaPosts = () => {
       }
     }
     getPosts();
-  }, []);
+  }, [props.url]);
 
   if (loading) {
     return <LoadingDesenho style={{ backgroundColor: "red" }} />;
