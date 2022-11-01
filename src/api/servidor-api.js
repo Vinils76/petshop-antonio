e8ef7805ec237c22b48566e457a52b815eddad13
Vinils76 <antoniovinicius7670@gmail.com>
2022-11-01 09:27:47 -0300

    configurções para acesso via rede

--- src/api/servidor-api.js
+++ src/api/servidor-api.js
@@ -1,3 +1,4 @@
 const porta = "2112";
-const serverApi = `http://localhost:${porta}`;
+//const serverApi = `http://localhost:${porta}`; versão localhost
+const serverApi = `http://10.20.45.33:${porta}`;
 export default serverApi;
